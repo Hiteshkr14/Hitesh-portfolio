59cd606c5a616c6a3b84c9aa0c4c176026f72e1a
Hitesh Kumar <krhitesh8@gmail.com>
2025-06-08 19:24:02 +0530

    Fixed image path issues for public directory

--- src/components/contactSection/Right.jsx
+++ src/components/contactSection/Right.jsx
@@ -5,7 +5,7 @@ const Right = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-6 lg:w-1/2">
       <img
-        src="../../public/images/email-image.png"
+        src="/images/email-image.png"
         alt="Contact"
         className="max-w-[350px] rounded-lg shadow-lg"
       />
